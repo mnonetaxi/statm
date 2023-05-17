4b926c9ee7a28d0fdb324e96ac5406a9c29cf5d0
unknown <mnonelelinquma91@gmail.com>
2023-05-17 08:33:21 +0200

    add Container,width

--- src/component/Video.jsx
+++ src/component/Video.jsx
@@ -4,7 +4,7 @@ import sample from "../video/Get-booted-2.mp4";
 const Video = () => {
   return (
     <div className="Container">
-      <video width="1200" className="videoTag" autoPlay loop muted>
+      <video width="1300" className="videoTag" autoPlay loop muted>
         <source src={sample} type="video/mp4" />
       </video>
     </div>
